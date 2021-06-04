9603246b58567b72576e80f28bfec5cdf5a88600
VR Voyage <avatarsetup_voyage@miouyouyou.fr>
2021-06-04 21:47:55 +0200

    Helper tools for NeosVR QAS : Blendshapes to DynVar
    My understanding of NeosVR programming tools is fairly low,
    so is my patience with the in-game tools.
    
    I don't know how to search for Blendshapes through Logix,
    however, Logix can search for dynamic variables and set
    them after.
    So I'm preparing a little plugin to help automate the
    process of creating dynamic variables to drive blendshapes.
    
    Then I should be able to read emotions configurations, generate
    logix programs that setup these emotions through the dynamic
    variables and then access these logix programs through a menu.
    
    Signed-off-by: VR Voyage <avatarsetup_voyage@miouyouyou.fr>

---
+++ Class1.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using BaseX;
+using FrooxEngine;
+
+namespace NeosQuickSetupTools
+{
+    [Category("MyHamsterIsRich")]
+    public class Class1 : Component
+    {
+
+        public readonly RelayRef<IField<float>> blah;
+        public RelayRef<IField<float>> bloh;
+        public readonly FieldDrive<Slot> fieldSlotRO;
+        public FieldDrive<bool> a;
+        public readonly bool b;
+        public readonly Sync<bool> c;
+        //public readonly Sync<Slot> syncSlotRO;
+        //public readonly RelayRef<IField<Slot>> relaySlotRO;
+        //public readonly Sync<SkinnedMeshRenderer> syncRenderer;
+        public readonly SyncRef<SkinnedMeshRenderer> skinRef;
+        public readonly SyncRef<Slot> otherSlot;
+        
+
+        protected override void OnAttach()
+        {
+            base.OnAttach();
+            UniLog.Log("Help I'm attached ! Send someone !");
+            UniLog.Log("My Slot Name is : " + Slot.Name);
+           
+            
+        }
+
+        private DynamicValueVariableDriver<T> AddDynVarFor<T>(
+            IField<T> field,
+            string name,
+            Slot s,
+            bool forceLink = false)
+        {
+            DynamicValueVariableDriver<T> dynVariable =
+                s.AttachComponent<DynamicValueVariableDriver<T>>();
+            dynVariable.VariableName.Value = name;
+            dynVariable.Target.Target = field;
+            if (dynVariable.Target.Target == null)
+            {
+                if (!forceLink)
+                {
+                    s.RemoveComponent(dynVariable);
+                    dynVariable = null;
+                }
+                else
+                {
+                    /* FIXME ! Untested ! */
+                    dynVariable.Target.ForceLink(field);
+                }
+            }
+            return dynVariable;
+        }
+
+        private DynamicValueVariableDriver<T> AddDynVarFor<T>(
+            IField<T> field,
+            string name,
+            bool forceLink = false)
+        {
+            return AddDynVarFor(
+                field,
+                name,
+                field.FindNearestParent<Slot>(),
+                forceLink);
+        }
+
+        protected override void OnChanges()
+        {
+            base.OnChanges();
+            UniLog.Log("-----------------");
+            UniLog.Log("Changed triggered");
+
+            
+
+            //if (body != null && body.IsValid)
+            if (skinRef             != null 
+                && skinRef.Target   != null
+                && otherSlot        != null
+                && otherSlot.Target != null)
+            {
+                UniLog.Log("Skin !");
+                UniLog.Log(skinRef);
+                
+                SkinnedMeshRenderer skin = skinRef.Target;
+                //Slot s = otherSlot.Target;
+                for (int i = 0; i < skin.BlendShapeCount; i++)
+                {
+                    string name = skin.BlendShapeName(i);
+                    IField<float> blendShape = skin.GetBlendShape(name);
+                    AddDynVarFor(blendShape, "blendshape." + name, otherSlot);
+
+                }
+            }
+            /*if (slot != null && slot.Target.Value != null)
+            {
+                UniLog.Log("Slot target value");
+                UniLog.Log(slot.Target.Value);
+
+                Slot bodySlot = slot.Target.Value;
+                SkinnedMeshRenderer skin = bodySlot.GetComponent<SkinnedMeshRenderer>();
+                IField<float> blendShapeField = skin.GetBlendShape("vrc.v_aa");
+                 blendShapeField.DriveFromVariable("vrc.v_aa");
+            }*/
+        }
+
+    }
+}
